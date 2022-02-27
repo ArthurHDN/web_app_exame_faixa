eda4c7eba25f45c785afe38ba6fba56e59a3c77b
Arthur <arthurnunes.petee@gmail.com>
2022-02-27 17:42:37 -0300

    Init MySQL python API

--- server/mysqlapi.py
+++ server/mysqlapi.py
@@ -1,3 +1,12 @@
+import mysql.connector
 
 class MySQLAPI():
-    pass
+    def __init__(self, host, database, user, password):
+        self._connection = mysql.connector.connect(host=host,
+            database=database,
+            user=user,
+            password=password
+        )
+
+    def get_connection(self):
+        return self._connection
