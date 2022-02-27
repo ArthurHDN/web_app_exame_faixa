a366fa5564f11b18ea26c05cb0e6eef17f135249
Arthur <arthurnunes.petee@gmail.com>
2022-02-27 18:27:53 -0300

    Criar cadastros

--- server/cadastrador.py
+++ server/cadastrador.py
@@ -3,14 +3,21 @@ import grpc
 import cadastrador_pb2
 import cadastrador_pb2_grpc
 
+from mysqlapi import MySQLAPI
+
 
 class CadastradorServicer(cadastrador_pb2_grpc.CadastradorServicer):
     def __init__(self):
         print('Instancia Cadastrador Service criada com sucesso' + str(self))
+        self._mysql = MySQLAPI('bancodedados', 'didynDB', 'root', 'didyn')
 
     def Cadastra(self, request, context):
         print('Chamado o metodo de cadastra')
-        return cadastrador_pb2.DatabaseResponse(message = 'AAAAAAAAAAA')
+        table = 'cadastro'
+        fields = ('nome','gub')
+        values = (request.Nome,request.GUB)
+        self._mysql.insert(table, fields, values)
+        return cadastrador_pb2.DatabaseResponse(message = 'OK')
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
--- server/mysqlapi.py
+++ server/mysqlapi.py
@@ -7,6 +7,12 @@ class MySQLAPI():
             user=user,
             password=password
         )
+        self._cursor = self._connection.cursor()
+
+    def insert(self, table, fields, values):
+        cmd_sql = f"INSERT INTO {table} ({','.join([str(s) for s in fields])}) VALUES ({','.join(['%s' for s in fields])})"
+        self._cursor.execute(cmd_sql, values)
+        self._connection.commit()
 
     def get_connection(self):
         return self._connection
