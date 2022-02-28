73a29c8cf9efcae0390cbf67fbb2176482985f9b
MariaLuiza <ma.luizalves@gmail.com>
2022-02-28 08:06:55 -0300

    update db

--- build/bancodedados/init_db.sql
+++ build/bancodedados/init_db.sql
@@ -5,44 +5,45 @@ USE didynDB;
 
 CREATE TABLE IF NOT EXISTS escola(
   id_escola INT(11) UNIQUE AUTO_INCREMENT,
-  nome VARCHAR(255),
+  nome VARCHAR(255) NOT NULL,
   endereco VARCHAR(255),
   PRIMARY KEY (id_escola)
+  -- test
 );
 
 CREATE TABLE IF NOT EXISTS aluno(
   id_aluno INT(11) UNIQUE AUTO_INCREMENT,
-  id_escola INT(11),
-  nome VARCHAR(255),
-  gub VARCHAR(255),
+  id_escola INT(11) NOT NULL,
+  nome VARCHAR(255) NOT NULL,
+  gub INT(11) NOT NULL,
   professor VARCHAR(255),
   PRIMARY KEY (id_aluno),
-  FOREIGN KEY (id_escola) REFERENCES escola(id_escola)
+  FOREIGN KEY (id_escola) REFERENCES escola(id_escola) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS avaliacao(
-  id_avalicao INT(11) UNIQUE AUTO_INCREMENT,
-  id_aluno INT(11),
-  mestre VARCHAR(255),
-  gub VARCHAR(255),
-  sagui INT(11),
-  sonkisul INT(11),
-  balkisul INT(11),
-  sibon_donjak_1 INT(11),
-  sibon_donjak_2 INT(11),
-  comb_tec_1 INT(11),
-  comb_tec_2 INT(11),
-  comb_tec_3 INT(11),
-  taeguk_1 INT(11),
-  taeguk_2 INT(11),
-  taeguk_3 INT(11),
-  matchuho_kyorugui INT(11),
-  kyorugui INT(11),
-  kyopa INT(11),
-  avalicao_periodica INT(11),
-  avalicao INT(11),
+  id_avaliacao INT(11) UNIQUE AUTO_INCREMENT,
+  id_aluno INT(11) NOT NULL,
+  mestre VARCHAR(255) NOT NULL,
+  gub VARCHAR(255) NOT NULL,
+  sagui INT(11) NOT NULL,
+  sonkisul INT(11) NOT NULL,
+  balkisul INT(11) NOT NULL,
+  sibon_donjak_1 INT(11) NOT NULL,
+  sibon_donjak_2 INT(11) NOT NULL,
+  comb_tec_1 INT(11) NOT NULL,
+  comb_tec_2 INT(11) NOT NULL,
+  comb_tec_3 INT(11) NOT NULL,
+  taeguk_1 INT(11) NOT NULL,
+  taeguk_2 INT(11) NOT NULL,
+  taeguk_3 INT(11) NOT NULL,
+  matchuho_kyorugui INT(11) NOT NULL,
+  kyorugui INT(11) NOT NULL,
+  kyopa INT(11) NOT NULL,
+  avalicao_periodica INT(11) NOT NULL,
+  avalicao INT(11) NOT NULL,
   PRIMARY KEY (id_avaliacao),
-  FOREIGN KEY (id_aluno) REFERENCES aluno(id_aluno)
+  FOREIGN KEY (id_aluno) REFERENCES aluno(id_aluno) ON DELETE CASCADE 
 );
 -- CREATE TABLE IF NOT EXISTS users(
 --   id INT(11) UNIQUE AUTO_INCREMENT,
