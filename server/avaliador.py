31f086d016d5d19f39d1db2d13d51d78eb84e276
Arthur <arthurnunes.petee@gmail.com>
2022-03-23 16:57:59 -0300

    Avaliador remove e cria avaliacao

--- server/avaliador.py
+++ server/avaliador.py
@@ -7,11 +7,31 @@ from mysqlapi import MySQLAPI, TABLE_ALUNO, TABLE_AVALIACAO, TABLE_ESCOLA
 
 
 class AvaliadorServicer(avaliador_pb2_grpc.AvaliadorServicer):
+
     def __init__(self):
         self._mysql = MySQLAPI('bancodedados', 'didynDB', 'root', 'didyn')
 
+    def CriaAvaliacao(self, request, context):
+        table = TABLE_AVALIACAO
+        fields = ('id_aluno', 'mestre')
+        values = (request.aluno.id, request.mestre)
+        id = self._mysql.insert(table, fields, values)
+        return avaliador_pb2.DatabaseResponse(message = 'Inserido ID = ' + str(id))
+
     def InsereNota(self, request, context):
-        return avaliador_pb2.DatabaseResponse()
+        table = TABLE_AVALIACAO
+        field = request.nome
+        value = request.valor
+        filter_ = f"id_aluno = {request.aluno.id}"
+        rowcount = self._mysql.update(table, field, value, filter_)
+        return avaliador_pb2.DatabaseResponse(message = 'Linhas afetadas = ' + str(rowcount))
+
+    def RemoveAvaliacao(self, request, context):
+        id = request.id
+        table = TABLE_AVALIACAO
+        filter = 'id_avaliacao = ' + str(id)
+        self._mysql.delete(table, filter)
+        return avaliador_pb2.DatabaseResponse(message = 'Removido ID = ' + str(id))
 
 
 def serve():
